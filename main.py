04974cd5db89e0731fbee37bb0c97237a0da3dc1
chansigit <chansigit@gmail.com>
2017-04-27 00:17:18 +0800

    clean directory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from LIDCHelper.PatientClass import Patient
-from SlidingWindowClipping.filter import winFilter
+from LIDCHelper.PatientClass import *#Patient
 patientPath= 'E:/code/dicom_data/1.3.6.1.4.1.14519.5.2.1.6279.6001.179049373636438705059720603192'
 p=Patient(patientPath)
 print((p.ZCoords))
